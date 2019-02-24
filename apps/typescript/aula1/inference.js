cb3a33aa319a3d0cc5c2787d673b6a82644f1776
leticiaRibeiro <leticia.cribeiro317@gmail.com>
2019-02-23 23:06:34 -0300

    Automatic build by creating a task on VSCode

--- apps/typescript/aula1/inference.js
+++ apps/typescript/aula1/inference.js
@@ -1,5 +1,12 @@
-// utilizando a inferencia de tipos no typescript
+/*
+Utilizando a inferencia de tipos no typescript
+*/
 function showLog(message) {
     console.log(message);
 }
 showLog('Hello World');
+showLog(23);
+var test = ['Hello', 'Test'];
+var isEnabled = true;
+var testw = ['Hello'];
+//# sourceMappingURL=inference.js.map
